5eae30959e0fc655d6c7ac525ffeead8ad2936ae
wanghongyu <375989120@qq.com>
2022-03-29 18:15:24 +0800

    feedback

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -1,60 +1,104 @@
 <template>
-  <!-- 一条文章单元格 -->
-  <van-cell>
-    <!-- 标题区域的插槽 -->
-    <template #title>
-      <div class="title-box">
-        <!-- 标题 -->
-        <span>{{ artObj.title }}</span>
-        <!-- 单图 -->
-        <img v-if="artObj.cover.type === 1" class="thumb" :src="artObj.cover.images[0]" alt="">
-      </div>
-      <!-- 多图 -->
+  <div>
+    <!-- 一条文章单元格 -->
+    <van-cell>
+      <!-- 标题区域的插槽 -->
+      <template #title>
+        <div class="title-box">
+          <!-- 标题 -->
+          <span>{{ artObj.title }}</span>
+          <!-- 单图 -->
+          <img
+            v-if="artObj.cover.type === 1"
+            class="thumb"
+            :src="artObj.cover.images[0]"
+            alt=""
+          />
+        </div>
+        <!-- 多图 -->
         <div v-if="artObj.cover.type > 1" class="thumb-box">
-          <img class="thumb"
+          <img
+            class="thumb"
             v-for="(imgUrl, index) in artObj.cover.images"
             :key="index"
             :src="imgUrl"
             alt=""
-          >
+          />
         </div>
-    </template>
-    <template #label>
-      <div class="label-box">
-        <div>
-          <span>{{ artObj.aut_name }}</span>
-          <span>{{ artObj.comm_count }}评论</span>
-          <span>{{ formatTime(artObj.pubdate) }}</span>
+      </template>
+      <template #label>
+        <div class="label-box">
+          <div>
+            <span>{{ artObj.aut_name }}</span>
+            <span>{{ artObj.comm_count }}评论</span>
+            <span>{{ formatTime(artObj.pubdate) }}</span>
+          </div>
+          <!-- 反馈按钮 -->
+          <van-icon name="cross" @click="showAction" />
         </div>
-        <!-- 反馈按钮 -->
-        <van-icon name="cross" />
-      </div>
-    </template>
-  </van-cell>
+      </template>
+    </van-cell>
+    <van-action-sheet
+      v-model="show"
+      :actions="actions"
+      @select="onSelect"
+      get-container="body"
+      :cancel-text="bottomText"
+      @cancel="cancelFn"
+      @close="closeFn"
+    />
+  </div>
 </template>
 
 <script>
-import { timeAgo } from '@/utils/date.js'
-// import { firstActions, secondActions } from '@/api/report.js'
+import { timeAgo } from "@/utils/date.js";
+import { firstActions, secondActions } from "@/api/report.js";
 export default {
   props: {
     artObj: Object,
     isShow: {
       type: Boolean,
-      default: true
-    }
+      default: true,
+    },
   },
-  data () {
+  data() {
     return {
       show: false, // 反馈面板显示/隐藏
-      // actions: firstActions, // 反馈面板选项数组(套对象)
-      bottomText: '取消' // 底部按钮的文字
-    }
+      actions: firstActions, // 反馈面板选项数组(套对象)
+      bottomText: "取消", // 底部按钮的文字
+    };
   },
   methods: {
-    formatTime: timeAgo // 函数体是timeAgo
-  }
-}
+    formatTime: timeAgo, // 函数体是timeAgo
+    showAction() {
+      this.show = true;
+    },
+    onSelect(action, item) {
+      if (action.name === "反馈垃圾内容") {
+        this.actions = secondActions;
+        this.bottomText = "返回";
+      } else if (action.name === "不感兴趣") {
+        this.$emit("disLikeEV", this.artObj.art_id);
+        this.show = false; // 无论成功/失败-直接让反馈面板隐藏
+      } else {
+        // 二级反馈选项
+        this.$emit("reportEV", this.artObj.art_id, action.value);
+        this.show = false; // 关闭-下面cancel会帮你把反馈选项切回来
+      }
+    },
+    cancelFn () {
+      if (this.bottomText === '返回') {
+        this.show = true // 强制让它回来
+        this.actions = firstActions
+        this.bottomText = '取消'
+      }
+    },
+    closeFn () {
+      this.actions = firstActions
+      this.bottomText = '取消'
+    }
+  },
+};
 </script>
 
 <style scoped lang="less">
--- src/views/Home/components/ArticleList.vue
+++ src/views/Home/components/ArticleList.vue
@@ -1,54 +1,93 @@
 <template>
   <div>
-    <van-list
-      v-model="loading"
-      :finished="finished"
-      finished-text="没有更多了"
-      @load="onLoad"
-    >
-      <ArticleItem
-        v-for="obj in list"
-        :key="obj.art_id"
-        :artObj="obj"
-      ></ArticleItem>
-    </van-list>
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+        :immediate-check="false"
+        offset="50"
+      >
+        <ArticleItem
+          v-for="obj in list"
+          :key="obj.art_id"
+          :artObj="obj"
+          @disLikeEV="disLikeFn"
+          @reportEV="reportFn"
+        ></ArticleItem>
+      </van-list>
+    </van-pull-refresh>
   </div>
 </template>
 
 <script>
-import ArticleItem from "../../../components/ArticleItem.vue"
-import { getAllArticleListAPI } from '@/api'
+import ArticleItem from "../../../components/ArticleItem.vue";
+import { getAllArticleListAPI, dislikeArticleAPI, reportArticleAPI } from '@/api'
+import Notify from '@/ui/Notify.js'
+
 export default {
   components: {
-    ArticleItem
+    ArticleItem,
   },
   data() {
     return {
       list: [],
       loading: false,
-      finished: false
-    }
+      finished: false,
+      timestamp: new Date().getTime(),
+      isLoading: false,
+    };
   },
   props: {
-    channelId: Number
+    channelId: Number,
   },
   created() {
-    this.getArticle()
+    this.getArticle();
   },
   methods: {
     async getArticle() {
       const res = await getAllArticleListAPI({
         channel_id: this.channelId,
-        timestamp: (new Date()).getTime()
-      })
-      this.list = res?.data?.data?.results || []
-    },
-    onLoad() {}
-  }
+        timestamp: this.timestamp,
+      });
+      this.list = [...this.list, ...res.data.data.results];
+      this.timestamp = res.data.data.pre_timestamp;
 
-}
+      this.loading = false;
+      if (!res.data.data.pre_timestamp) this.finished = true;
+      this.isLoading = false
+    },
+    onLoad() {
+      this.getArticle();
+    },
+    onRefresh() {
+      this.list = []
+      this.timestamp = new Date().getTime()
+      this.getArticle()
+    },
+    async disLikeFn (id) {
+      try {
+        await dislikeArticleAPI({
+          artId: id
+        })
+        Notify({ type: 'success', message: '反馈成功' })
+        console.log('成功了')
+      } catch (err) {
+        console.log('失败了')
+      }
+    },
+    // 反馈-垃圾内容
+    async reportFn (id, value) {
+      await reportArticleAPI({
+        artId: id,
+        type: value
+      })
+      Notify({ type: 'success', message: '举报成功' })
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>

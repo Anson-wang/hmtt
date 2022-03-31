2a1c8b001e72edeebc435f817052b194687d46de
wanghongyu <375989120@qq.com>
2022-03-31 12:14:01 +0800

    item

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -34,7 +34,7 @@
             <span>{{ formatTime(artObj.pubdate) }}</span>
           </div>
           <!-- 反馈按钮 -->
-          <van-icon name="cross" @click="showAction" />
+          <van-icon name="cross" @click="showAction" v-if="isShow" />
         </div>
       </template>
     </van-cell>
